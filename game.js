1b0ea2433b7142a4a4af19a64fc0e0e0b342cfc7
Glitch (maprunnerdev) <none>
2019-03-25 22:09:06 +0000

    Rewound to commit 17c693d2e177c325c7d84edf27408edf1ce7fa23 + 0 edits

--- game.js
+++ game.js
@@ -2081,7 +2081,7 @@ function Game(M){
   this.canvas = document.createElement('canvas')
   this.canvas.id = 'canvas'
   this.canvas.tabindex = 1
-  
+  this.canvas.innerHTML = "You browser is too old to use canvas technologies."
   divGame.appendChild(this.canvas)
   this.c = this.canvas.getContext('2d');
   
@@ -2640,28 +2640,6 @@ function Game(M){
       if(game.resetAnimate) game.resetAnimate = false
       if(game.playing[0]) window.requestAnimationFrame(game.animate);
   }
-  
-  this.start = () => {
-    this.mode = 'play'
-    this.init()
-    this.playing = [true, true]
-    this.cfg.updateAll = true
-
-    this.resetAnimate = true
-    this.animate()
-    this.window.play.start()
-  }
-  
-  this.edit = () => {
-    this.mode = 'edit'
-    this.init()
-    
-    this.playing = [true, true]
-    this.cfg.updateAll = true
-
-
-    this.animate()
-    this.window.edit.start()
 }
 
 
@@ -2684,7 +2662,7 @@ function Game(M){
 
 //     Runner.resetAnimate = true
 //     Runner.animate()
-//     Runner.window.play.start()
+//     Runner.window[Runner.mode].start()
 //       btns.forEach(b => b.destroy())
 //   })
 
@@ -2738,7 +2716,6 @@ function Game(M){
         position: absolute;
         text-align: center;
         top: 0;
-        left: 0;
         width:100%;
         padding: 10px 0;
         color: white;
